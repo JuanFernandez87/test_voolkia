6d060058f293bf20f6047db05e3fa9cc1a8c5d18
Juan Fernandez <77213272+JuanFernandez87@users.noreply.github.com>
2022-02-11 10:41:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ SITE_ID = "MLA"
 
 if __name__ == '__main__':
     '''
-    Funcion que recorre todos los ítems publicados por el 
+    Llama a la funcion que recorre todos los ítems publicados por el 
     seller_id = 179571326 del site_id = "MLA"
-    Genera un archivo de LOG que contiene los siguientes datos de
-    cada ítem: 
-    - "id" del ítem
-    - "title" del item
-    - "category_id" donde está publicado
     '''
     query.query_api(SELLER_ID, SITE_ID)
